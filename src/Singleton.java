753ed375682b598502dec48b5625fb33642aeb59
Administrator <Administrator@10.107.21.24>
2015-11-12 12:28:26 +0800

    Singleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -22,6 +22,74 @@ public class Singleton{
 	}
 }
 
+/***
+ * 
+ */
+/*public class Singleton{
+	private Singleton(){
+		
+	}
+	private static Singleton instance=new instance();
+	public Singleton getInstance()
+	{
+		return instance;
+	}
+}*/
+/**
+ * Singleton
+ */
+/*public class Singleton{
+	private Singleton(){
+		
+	}
+	private static Singleton instance=new Singleton();
+	public Singleton getInstance()
+	{
+		return instance;
+	}
+}
+*/
+
+//
+/*public class Singleton{
+	private Singleton()
+	{
+		
+	}
+	private static Singleton instance=new Singleton();
+	public void getSingleton()
+	{
+		return instance;
+	}
+}*/
+/*public class Singleton()
+{
+	private Singleton(){
+		
+	}
+	private static Singleton instance=new Singleton();
+	public void getSingelton()
+	{
+		return instance;
+		
+	}
+}
+*/
+
+/*create databse db;
+create table student{
+	id not null  auto_increment primary key,
+	name varchar(50),
+	age int(10),
+};
+create database db;
+create table student{
+	id not null auto_increment primary key,
+	name varchar(50),
+	age int(10),
+};
+*/
+
 
 /*public class Singleton{
 	private Singleton(){
